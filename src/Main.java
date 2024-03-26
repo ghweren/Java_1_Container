6276809e4ff1b5c367aa8290e1d2c10a912be33c
ghweren <android1190@list.ru>
2024-03-26 23:08:06 +0300

    First implementation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-public class Main {
+import java.util.Vector;
 
+public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
---
+++ src/MyIntVector.java
@@ -0,0 +1,138 @@
+public class MyIntVector {
+    private int size;
+    private int[] arr;
+    private int capacity=10;
+
+    private void increase_the_capacity(int capacity)
+    {
+        int[] temp = arr.clone();
+        capacity*=2;
+        arr = new int[capacity*2];
+        for(int i=0;i<size;i++)
+            arr[i]=temp[i];
+    }
+
+    public MyIntVector() {
+        size=0;
+        capacity=10;
+        arr=new int[capacity];
+    }
+    public MyIntVector(int size)
+    {
+        this.size=size;
+        if(size>0.5*capacity)
+            capacity=2*size;
+        else capacity=10;
+        arr=new int[capacity];
+    }
+
+    public void push_back(int element)
+    {
+        size++;
+        if(size>capacity*0.8)
+            increase_the_capacity(capacity*2);
+        arr[size-1]=element;
+    }
+
+    public  void push_front(int element)
+    {
+        size++;
+        if(size>0.8*capacity)
+            increase_the_capacity(capacity*2);
+        int[] temp = arr.clone();
+        arr=new int[capacity];
+        arr[0]=element;
+        for(int i=1;i<size;i++)
+            arr[i]=temp[i-1];
+    }
+
+    public int pop_back()
+    {
+        int[] temp=arr.clone();
+        arr=new int[capacity];
+        size--;
+        for(int i =0;i<size;i++)
+            arr[i]=temp[i];
+        return  temp[size+1];
+    }
+
+    public int pop_front()
+    {
+        int[] temp=arr.clone();
+        arr=new int[capacity];
+        size--;
+        for(int i =1;i<size+1;i++)
+            arr[i-1]=temp[i];
+        return  temp[0];
+    }
+
+    public void add(int element,int index)
+    {
+        if(index>size-1)
+            System.out.println("The entered index goes beyond to array");
+        else
+        {
+            size++;
+            if(size>0.8*capacity)
+                increase_the_capacity(capacity*2);
+            int[] temp = arr.clone();
+            arr[index]=element;
+            for(int i=index+1;i<size;i++)
+                arr[i]=temp[i-1];
+        }
+    }
+    public void replace(int element,int index)
+    {
+        if(index>size-1)
+            System.out.println("The entered index goes beyond to array");
+        else
+        {
+            arr[index]=element;
+        }
+    }
+
+    public void delete(int index)
+    {
+        if(index>size-1)
+            System.out.println("The entered index goes beyond to array");
+        else
+        {
+            int[] temp=arr.clone();
+            size--;
+            arr=new int[capacity];
+            for(int i=0;i<index;i++)
+                arr[i]=temp[i];
+            for(int i =index;i<size+1;i++)
+                arr[i]=temp[i+1];
+        }
+    }
+    public int getSize()
+    {
+        return size;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public int get(int index)
+    {
+            return arr[index];
+    }
+
+    public void resize(int size)
+    {
+        if(size>0.5*capacity)
+            increase_the_capacity(size*2);
+        int[] temp =arr.clone();
+        arr=new int[capacity];
+        if(this.size>size)
+            for(int i=0;i<size;i++)
+                arr[i]=temp[i];
+        else
+            for(int i=0;i<this.size;i++)
+                arr[i]=temp[i];
+        this.size=size;
+    }
+}
