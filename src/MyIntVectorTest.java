2d979d7823b15d54719281a049c147cc280c2ffe
ghweren <android1190@list.ru>
2024-03-27 13:14:28 +0300

    Added unitTest for push_front

--- src/MyIntVectorTest.java
+++ src/MyIntVectorTest.java
@@ -45,7 +45,32 @@ class MyIntVectorTest {
 
 
     @org.junit.jupiter.api.Test
-    void push_front() {
+    void push_front_then_empty() {
+        testVector.push_front(1);
+        assertArrayEquals(new int[]{1},testVector.toArray());
+        assertEquals(1,testVector.getSize());
+        assertEquals(10,testVector.getCapacity());
+    }
+
+    @Test
+    void often_push_front()
+    {
+        testVector.push_front(1);
+        testVector.push_front(2);
+        testVector.push_front(3);
+        assertArrayEquals(new int[]{3,2,1},testVector.toArray());
+        assertEquals(3,testVector.getSize());
+        assertEquals(10,testVector.getCapacity());
+    }
+
+    @Test
+    void push_front_then_higher_capacity()
+    {
+        for(int i = 0;i<9;i++)
+            testVector.push_front(i);
+        assertArrayEquals(new int[]{8,7,6,5,4,3,2,1,0},testVector.toArray());
+        assertEquals(9,testVector.getSize());
+        assertEquals(20,testVector.getCapacity());
     }
 
     @org.junit.jupiter.api.Test
